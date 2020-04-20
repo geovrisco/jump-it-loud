d47f5c82480a080249be40b2e8595ea2d6cfb6fe
geovrisco <geovrisco@gmail.com>
2020-04-20 15:56:21 +0700

    testing

--- App/App.js
+++ App/App.js
@@ -1,5 +1,5 @@
-import React,{useEffect,useState,Component} from 'react';
-import { StyleSheet, Text, View, StatusBar,TouchableOpacity ,Button} from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, StatusBar,TouchableOpacity ,Button, Image} from 'react-native';
 import RNSoundLevel from 'react-native-sound-level'
 import Constants from './gameSettings/constants'
 import {GameEngine} from 'react-native-game-engine'
@@ -8,6 +8,10 @@ import * as Permissions from 'expo-permissions'
 import Character from './components/character'
 import Physics from './gameSettings/physics'
 import Floor from './components/floor'
+import Background from './assets/background.png'
+import Obstacle from './components/obstacle';
+
+
 
 
 export default class App extends Component {
@@ -16,19 +20,21 @@ export default class App extends Component {
     this.gameEngine = null;
     this.entities = this.setupWorld()
     this.state = {
-      rawValue:0,
-      valueInDb:0
+      isRunning:false,
+      score:0
     }
   }
 
   async getPermission(){
     const {status, expires, permissions} = await Permissions.askAsync(Permissions.AUDIO_RECORDING)
     if (status !== 'granted'){
-      
+      alert('This Wonderful application need audio recording permission to run on your phone')
     }else {
       console.log('sukses')
+      
     }
   }
+
   
   setupWorld = () =>{
     // console.log(Matter)
@@ -36,50 +42,89 @@ export default class App extends Component {
     let engine = Matter.Engine.create({ enableSleeping:false})
     let world = engine.world;
     // console.log(world)
-    let character = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT-100, 50,50)
-    let floor  = Matter.Bodies.rectangle(Constants.MAX_WIDTH/2, Constants.MAX_HEIGHT - 90, Constants.MAX_WIDTH,50, {isStatic:true})
-    Matter.World.add(world, [character,floor]) 
+    let character = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT-200, 50,50,{label:'character'})
+    let floor  = Matter.Bodies.rectangle(Constants.MAX_WIDTH/2, Constants.MAX_HEIGHT - 50, Constants.MAX_WIDTH,50, {isStatic:true, label:'floor'})
+    let obstacle = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT-102, 30,30, {label:"obstacle"})
+    Matter.World.add(world, [character,floor,obstacle]) 
+
+    Matter.Events.on(engine,"collisionStart", event => {
+      // console.log(event)
+      let pairs = event.pairs
+      let objA = pairs[0].bodyA.label
+      let objB = pairs[0].bodyB.label
+      // let objC = pairs[0].bodyC.label
+      if(objA==='obstacle' && objB==='character'){
+        // console.log(objA,objB,'nabrakcoy')
+        this.gameEngine.dispatch({type:'gameOver'})
+      }else if (objA==='character' && objB==='obstacle'){
+        // console.log(objA,objB,'nabrakcoy2')
+        this.gameEngine.dispatch({type:'gameOver'})
+      }
+    
+    })
 
     return{
       physics: {engine, world, RNSoundLevel},
       character: {body:character, size:[50,50], color:"blue", renderer: Character},
-      floor: {body:floor, size:[Constants.MAX_WIDTH,50], color:"red", renderer: Floor}
+      floor: {body:floor, size:[Constants.MAX_WIDTH,50], color:"red", renderer: Floor},
+      obstacle : {body:obstacle, size:[30,30], color:"red", renderer: Obstacle},
     }
+
+
   }
 
+  
 
+  componentWillUnmount(){
+    RNSoundLevel.stop()
+  }
 
   componentDidMount(){
     // RNSoundLevel.stop()
-    this.getPermission()
-    console.log('mulai')
     RNSoundLevel.start()
-    RNSoundLevel.onNewFrame = (data) =>{
-            this.setState({
-            rawValue:data.rawValue,
-            valueInDb:data.value
-          })
-      // console.log(this.state.rawValue)
+    this.getPermission()
+    console.log('mulai=============================')
+    this.setState({
+      isRunning:true
+    })
     }
-    // console.log(this.state)
+
+  onEvent = (e) =>{
+    console.log(e)
+    
+
+    if(e.type==='score'){
+      this.setState({
+        score:this.state.score+1
+      })
+      console.log(this.state.score)
+    }
+    if(e.type==='gameOver'){
+      console.log('gameOver')
+      this.setState({
+        isRunning:false
+      })
+      alert('game over dong coy')
+    }
+
   }
 
   render(){
     return(
       <View style={styles.container}>
+        <Image source={Background} style={styles.background} resizeMode="stretch">
+        </Image>
+          
         <GameEngine
           ref={(ref) => { this.gameEngine = ref}}
           style={styles.gameContainer}
           systems = {[Physics]}
           entities= {this.entities}
-          running={true}
+          running={this.state.isRunning}
+          onEvent={this.onEvent}
         />
-        <View style={{position:"absolute", top:30,left:0,bottom:0,right:0,flex:1}}>
-          <Button title="stop"
-            onPress={RNSoundLevel.stop}
-          >
-          </Button>
-        <Button title="start" onPress={RNSoundLevel.start}/>
+        <View style={{position:"absolute", top:30,left:30,bottom:0,right:0,flex:1}}>
+        <Text>{this.state.score}</Text>
         </View>
       </View>
     )
@@ -91,4 +136,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  background:{
+    position:"absolute",
+    top:0,
+    left:0,
+    right:0,
+    bottom:0,
+    width:Constants.MAX_WIDTH,
+    height:Constants.MAX_HEIGHT,
+  }
 });
--- App/gameSettings/physics.js
+++ App/gameSettings/physics.js
@@ -1,7 +1,7 @@
 import Matter from 'matter-js'
 import Constants from '../gameSettings/constants'
 
-const physics = (entities, {touches, time }) =>{
+const physics = (entities, {dispatch, time }) =>{
   let engine = entities.physics.engine;
   let character = entities.character.body
   let RNSoundLevel = entities.physics.RNSoundLevel
@@ -9,16 +9,16 @@ const physics = (entities, {touches, time }) =>{
   RNSoundLevel.onNewFrame = (data) => {
     // console.log(data)
     if(character.position.y > Constants.MAX_HEIGHT - 100) {
-      if(data.value > -20 && data.value <= -10){
+      if(data.value > -40 && data.value <= -15){
         Matter.Body.setVelocity(character, {x: character.velocity.x , y: -10})
-      }else if (data.value > -10){
+      }else if (data.value > -15){
         Matter.Body.setVelocity(character, {x: character.velocity.x , y: -15})
       }
     }
   }
 
   Matter.Engine.update(engine,time.delta)
-
+  dispatch({type:'score'})
 
 
   return entities;
