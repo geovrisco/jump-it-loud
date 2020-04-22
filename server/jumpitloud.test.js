c2b1d5e41e6e5e7a92a83681521ac8b06af86a0f
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2020-04-22 21:22:36 +0700

    menambahkan test case

--- server/jumpitloud.test.js
+++ server/jumpitloud.test.js
@@ -14,12 +14,24 @@ beforeAll(async () => {
 
 describe('get all leaderboard', function(){
     describe('successfully get leaderboard', function() {
-        it('Should return 200 and array of leaderboard', (done) => {
+        it('Should return 200', (done) => {
             request(app)
             .get('/leaderboard')
             .then(response => {
-                let {body, status} = response
+                let {status} = response
                 expect(status).toBe(200)
+                done()
+            }).catch(err =>{
+                done(err)
+            })
+        })
+    })
+    describe('successfully get leaderboard', function() {
+        it('Should have property leaderboard', (done) => {
+            request(app)
+            .get('/leaderboard')
+            .then(response => {
+                let {body} = response
                 expect(body).toHaveProperty('leaderBoard')
                 done()
             }).catch(err =>{
@@ -30,8 +42,8 @@ describe('get all leaderboard', function(){
 })
 
 describe('add leaderboard', function(){
-    describe('successfully add leaderboard', function(){
-        it('Should return 201 and an object(msg, data)', (done) => {
+    describe('successfully add leaderboard ', function(){
+        it('Should return 201', (done) => {
             request(app)
             .post('/leaderboard')
             .send({
@@ -39,11 +51,126 @@ describe('add leaderboard', function(){
                 score: 205
             })
             .then(response => {
-                let {body, status} = response
+                let {status} = response
                 expect(status).toBe(201)
-                expect(body).toHaveProperty('msg', 'Successfully add leaderboard')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Msg property should have Successfully add leaderboard', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
+                expect(body.msg).toBe('Successfully add leaderboard')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Should have property msg', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
+                expect(body).toHaveProperty('msg')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Should have property data', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
                 expect(body).toHaveProperty('data')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Should have property data, and data should have property name', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
+                expect(body.data).toHaveProperty('name')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Should have property data, and data should have property score', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
+                expect(body.data).toHaveProperty('score')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Should have property data, and data.name should be geo', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
                 expect(body.data.name).toBe('Geo')
+                done()
+            }).catch(err => {
+                done(err)
+            })
+        })
+    })
+    describe('successfully add leaderboard ', function(){
+        it('Should have property data, and data.score should be 205', (done) => {
+            request(app)
+            .post('/leaderboard')
+            .send({
+                name: 'Geo',
+                score: 205
+            })
+            .then(response => {
+                let {body} = response
                 expect(body.data.score).toBe(205)
                 done()
             }).catch(err => {
